47ad496f003d918e6c4a169dbb1d72f4144ccb0f
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-16 20:39:56 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TesteMateus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, temp;
+            Console.WriteLine("Informe um numero inteiro");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe outro numero inteiro");
+            n2 = int.Parse(Console.ReadLine());
+
+            temp = n1;
+            n1 = n2;
+            n2 = temp;
+
+            Console.WriteLine("SWAP {0} {1}", n1, n2);
+        }
+    }
+}
